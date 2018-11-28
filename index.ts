5c2b8f30c0b53299ac821aa18906ed30751c8c25
Ian Fabs <ianfabs@outlook.com>
2018-11-28 17:25:37 -0500

    Made core functionality simpler

--- index.ts
+++ index.ts
@@ -1,48 +1,29 @@
 import es6 from 'es6-promise';
 es6.polyfill();
-
 import 'isomorphic-fetch';
 
 interface JraphBasic{
-    url: string | Request;
+    url: string ;
     options: any;
-}
-abstract class JraphQuery implements JraphBasic{
-    url: string = "";
-    options: any = {};
-    query: string = "";
-    constructor(args: any){
-        Object.assign(this, args);
-    }
-}
-abstract class JraphMutation implements JraphBasic{
-    url: string = "";
-    options: any = {};
-    mutation: string = "";
-    constructor(args: any){
-        Object.assign(this, args);
-    }
+    query: string;
 }
 
-type JraphOptions = JraphQuery | JraphMutation;
+type JraphOptions = JraphBasic;
 
 async function jraph(argv: JraphOptions){
-    const url : string | Request = argv.url;
+    const url: string = argv.url;
     const headers = { 'Content-Type': 'application/json' };
-    const body = JSON.stringify( { query: ( (argv instanceof JraphQuery) ? argv.query : `mutation ${argv.mutation}`).replace(/([\\][n])?([\s])+/g, " ") } );
-    //(argv instanceof JraphQuery) ? {query: argv.query} : { query: `mutation ${argv.mutation.replace(/([\\][n])?([\s])+/g, " ")}`} );
+    const body = JSON.stringify( { query: (argv.query).replace(/([\\][n])?([\s])+/g, " ") } );
     const fetch_options = {
+        method: "POST",
+        //Defaults go before this line
+        ...argv.options,
+        //Perms go after
+        body,
         headers,
-        body: body,
-        ...argv.options
     };
-    console.log(body);
-    const request = await fetch(url, fetch_options).then(res=>res.json());
-    return request;
+    //const request = await fetch(url, fetch_options).then(res=>res.json());
+    return fetch(url, fetch_options).then(res=>res.json());
 }
-/* 
-* Some notes for the next version
-*   - Nevermind, I had notes but they were redundant.
-*/
+export { jraph, JraphOptions }
 export default jraph;
-export { jraph, JraphOptions, JraphQuery, JraphMutation }
