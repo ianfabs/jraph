83647e033f81f4949813d8e99e23fdccc34b5a33
Ian Fabs <ianfabs@outlook.com>
2019-04-03 20:28:05 -0400

    Made jraph better, by removing useless code

--- index.ts
+++ index.ts
@@ -1,31 +1,20 @@
-
 const jraph = (url: string, options: any) => {
-    const headers = { 'Content-Type': 'application/json' };
-    let fetch_options = {
-        method: "POST",
-        body: null,
-        headers,
-        //Defaults go before this line
-        ...options,
-        //Perms go after
-    };
-    return async (args: string[], ...values: any): Promise<any> =>{
-        let queryString = args.reduce( (ac, cv, ci) => ac + cv + values[ci] ).replace(/([\\][n])?([\s])+/g, " ");
-        fetch_options = {
-            ...fetch_options, 
-            body: JSON.stringify({ query: queryString }),
-        };
-        const request = await fetch(url, fetch_options);
-        try{
-            let json = await request.json();
-            return json;
-        }catch(error){
-            return {
-                request: await request.text(),
-                error,
-            };
-        }
-    };
+  return async (args: string[], ...vals: any[]): Promise<any> => {
+    const request = await fetch(url, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({query: args.reduce((a, v, i) => a + v + vals[i]).replace(/([\\][n])?([\s])+/g, " "),}),
+      ...options,
+    });
+    try {
+      return request.json();
+    } catch (error) {
+      return {
+        request: await request.text(),
+        error,
+      };
+    }
+  };
 }
 
 export { jraph };
